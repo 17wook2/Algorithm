572ccc5cd0e525e953c89333afbdaf307829cac4
17wook2 <59720037+17wook2@users.noreply.github.com>
2021-07-27 22:59:14 +0900

    백준 삼성기출

--- 백준/15684(사다리 조작).py
+++ 백준/15684(사다리 조작).py
@@ -1,46 +1,46 @@
 import copy
 n,m,h = list(map(int,input().split()))
-graph = [[0 for i in range(n+1)] for i in range(h+1)]
+graph = [[0 for i in range(n)] for i in range(h)]
 for i in range(m):
     a,b = list(map(int,input().split()))
-    graph[a][b] = 1
-    graph[a][b+1] = 1
+    graph[a-1][b-1] = 1
 
-def checkpoint(graph):
-    point = []
-    for i in range(1, h+1):
-        for j in range(1, n):
-            if graph[i][j] == 0 and graph[i][j + 1] == 0:
-                point.append((i, j))
-    return point
-
-def checkresult(graph):
-    for i in range(1,n+1): ## 1번줄부터 n번줄까지
+def check():
+    for i in range(n): ## n번줄까지
         start = i
-        for j in range(1,h+1):
-            if graph[j][start] == 1: ## 사다리를 만났다면
-                if graph[j][start-1] == 1:
-                    start -= 1
-                else:
-                    start += 1
+        for j in range(h):
+            if graph[j][start]: ## 사다리 시작점를 만났다면
+                start += 1
+            elif start > 0 and graph[j][start-1]: ## 왼쪽에 사다리가 있다면
+                start -= 1
         if i != start:
             return False
     return True
 
-def solve(graph, cnt):
-    if checkresult(graph):
-        return cnt
-    if cnt == 4:
-        return -1
-    else:
-        point = checkpoint(graph)
-        for p in point:
-            board = copy.deepcopy(graph)
-            board[p[0]][p[1]] = 1
-            board[p[0]][p[1]+1] = 1
-            solve(board, cnt+1)
-
+def solve(cnt,x,y):
+    global answer
+    if check():
+        answer = min(answer,cnt)
+        return
+    elif cnt == 3 or answer <= cnt:
+        return
+    for i in range(x,h):
+        if i == x: ## 같은 가로선에서 더 추가할 수 있는지 먼저 확인
+            k = y
+        else:
+            k = 0
+        for j in range(k,n):
+            if graph[i][j]:
+                continue
+            elif j > 0 and graph[i][j-1]:
+                continue
+            elif j > 0 and not graph[i][j-1]:
+                graph[i][j-1] = 1
+                solve(cnt+1,i,j+1)
+                graph[i][j-1] = 0
 answer = 4
 
-
-print(solve(graph,0))
+solve(0,0,0)
+if answer >= 4:
+    answer = -1
+print(answer)
